2f0f9e9f7fa58f8392fef036258a39bebe96cf70
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-10-27 23:30:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,19 +1,19 @@
 #include<iostream>
 #include<vector>
 
+std::vector<int> dp(1001, 0);
+
 int main()
 {
-    int n;
-    std::cin>>n;
-    std::vector<int> dp(n + 1, 0);
-    
     dp[1] = 1;
     dp[2] = 2;
-    
-    for (int i = 3; i <= n; ++i)
+    dp[3] = 3;
+    for (int i = 4; i <= 1000; ++i)
     {
         dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
     }
+    
+    int n;
+    std::cin>>n;
     std::cout<<dp[n]<<std::endl;
-    return 0;
 }
