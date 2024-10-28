87863218055bda9bf10eef6b223774c0a99842c9
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-10-29 00:46:37 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,74 +1,73 @@
 #include<iostream>
-#include<queue>
-
-#define MAX 51
-
+#include<vector>
+#include<stack>
 using namespace std;
 
-int map[MAX][MAX] = { 0, };
-bool visited[MAX][MAX] = { false };
-
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0 ,-1 };
-
-int m; // 배추밭 가로 길이
-int n; // 배추밭 세로 길이
-int k; // 배추가 심어져 있는 위치의 개수 
-int testCaseNum; //테스트 케이스의 개수 
-int cnt = 0; // 필요한 배추지렁이의 수 
-
-void bfs(int x, int y) {
-	queue<pair<int, int>> q;
-	q.push(pair<int, int>(x, y));
-	visited[x][y] = true;
-	while (!q.empty()){
-		pair<int, int> cur = q.front();
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cur.first + dx[i];
-			int ny = cur.second + dy[i];
-
-			if (nx > -1 && ny > -1 && nx < m && ny < n && map[nx][ny] == 1 && visited[nx][ny] == false) {
-				visited[nx][ny] = true;
-				q.push(pair<int,int>(nx, ny));
-			}
-		}
-	}
-}
-
-void init() {
-	cnt = 0;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			map[i][j] = 0;
-			visited[i][j] = false;
-		}
-	}
-}
-
-int main() {
-	cin >> testCaseNum;
-
-	for (int i = 0; i < testCaseNum; i++) {
-		cin >> m >> n >> k;
-		for (int j = 0; j < k; j++) {
-			int temp;
-			int temp2;
-			cin >> temp >> temp2; // 배추가 심어져 있는 위치에 대한 좌표
-			map[temp][temp2] = 1;
-		}
-		for (int j = 0; j < m; j++) {
-			for (int t = 0; t < n; t++) {
-				if (map[j][t] == 1 && visited[j][t] == false) {
-					bfs(j, t);
-					cnt++;
-				}
-			}
-		}
-		cout << cnt;
-		cout << endl;
-		init();
-		
-
-	}
+vector<pair<int, int>> dir = 
+{/*상*/{-1, 0},/*하*/{1, 0},/*좌*/{0, -1},/*우*/{0, 1}};
+int main()
+{
+    int T;
+    std::cin>>T;
+    
+    //가로, 세로, 배추가 심어져 있는 위치의 개수
+    
+    for (int i = 0; i < T; ++i)
+    {
+        int M, N, K;
+        std::cin>>M>>N>>K;
+    
+        vector<vector<int>> map(N, vector<int>(M, 0));
+    
+        for(int i = 0; i < K; ++i)
+        {
+            int X, Y;
+            std::cin>>X>>Y;
+            map[Y][X] = 1;
+        }
+        
+        int count = 0;  
+        for (int y = 0; y < N; ++y)
+        {
+            for (int x = 0; x < M; ++x)
+            {
+                if (map[y][x] != 1)
+                {
+                    continue;
+                }
+                
+                stack<pair<int, int>> st;
+                ++count;
+                st.push({y, x});
+                map[y][x] = 0;
+                
+                while(!st.empty())
+                {
+                    auto pair = st.top();
+                    st.pop();
+                    int currentY = pair.first;
+                    int currentX = pair.second;
+                    
+                    for(auto& pair : dir)
+                    {
+                        int nextY = pair.first + currentY;
+                        int nextX = pair.second + currentX;
+                    
+                        if (nextY < 0 || map.size() <= nextY || nextX < 0 || map[0].size() <= nextX)
+                        {
+                            continue;
+                        }
+                    
+                        if (map[nextY][nextX] == 0)
+                        {
+                            continue;
+                        }
+                        map[nextY][nextX] = 0;
+                        st.push({nextY, nextX});
+                    }
+                }
+            }
+        }
+        std::cout<<count<<std::endl;
+    }
 }
