323df910f31ea6a2b5c9f81d0667e4af66eed473
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-10-31 17:30:35 +0900

    [Silver II] Title: 마인크래프트, Time: 192 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.cc
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -1,48 +1,56 @@
 #include<iostream>
-using namespace std;
+#include<vector>
+#include<limits>
 
-//	제거 2초
-//	채우기 1초
-
-int land[501][501];
-int main(void)
+int main()
 {
-	int mint = 99999999;
-	int minh = 99999999;
-	int time;
-	int N, M, B;
-	cin >> N >> M >> B;
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++)
-			cin >> land[i][j];
-
-	for (int h = 0; h <= 256; h++)
-	{
-		int remove = 0;
-		int stack = 0;
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < M; j++)
-			{
-				if (land[i][j] > h)		//	제거해야할 블록 개수
-					remove += land[i][j] - h;
-				else if (land[i][j] == h)		//	같으면 통과
-					continue;
-				else if (land[i][j] < h)		//	쌓아야 할 블록 개수
-					stack += h - land[i][j];
-			}
-		}
-		if (remove + B >= stack)		//	"제거해서 인벤토리에 추가한 블록 + 원래 있던 블록 >= 쌓아야할 블록 개수" 라면..
-		{
-			time = remove * 2 + stack;
-			if (time <= mint)
-			{
-				mint = time;
-				minh = h;		//	현재 설정된 높이가 최소높이가 됨
-			}
-		}
-	}
-	cout << mint << ' ' << minh;
-	return 0;
+    
+    int N,M,B;
+    std::cin>>N>>M>>B;
+    std::vector<std::vector<int>> map(N, std::vector<int>(M, -1));
+    for (int y = 0; y < N; ++y)
+    {
+        for (int x = 0; x < M; ++x)
+        {
+            std::cin>>map[y][x];
+        }
+    }
+    
+    int minTime = std::numeric_limits<int>::max();
+    int minHigh = std::numeric_limits<int>::max();
+    int time = 0;
+    for (int h = 0; h <= 256; ++h)
+    {
+        int remove = 0;
+        int stack = 0;
+        for (int y = 0; y < N; ++y)
+        {
+            for (int x = 0; x < M; ++x)
+            {
+                if (map[y][x] > h)
+                {
+                    remove += map[y][x] - h;
+                }
+                else if (map[y][x] == h)
+                {
+                    continue;
+                }
+                else if (map[y][x] < h)
+                {
+                    stack += h - map[y][x];
+                }
+            }
+        }
+        // 작업이 가능하다.
+        if (remove + B >= stack)
+        {
+            time = remove * 2 + stack;
+            if (time <= minTime)
+            {
+                minTime = time;
+                minHigh = h;
+            }
+        }
+    }
+    std::cout<<minTime<<' '<<minHigh;
 }
