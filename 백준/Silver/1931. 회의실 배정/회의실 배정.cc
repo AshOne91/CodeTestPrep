f5c03231d1e2ec104068929547c7e7b9c0d862c3
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-04 12:22:05 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,37 +1,48 @@
 #include<iostream>
-#include<stdio.h>
 #include<algorithm>
 #include<vector>
-using namespace std;
-
-int value[10];
 
 int main()
 {
-	int N, end, begin;
-
-	vector<pair<int, int>> schedule;
-
-	cin >> N ;
-
-	for (int i = 0; i < N; i++)
-	{
-		cin >> begin >> end;
-		schedule.push_back(make_pair(end, begin));
-	}
-	
-	sort(schedule.begin(), schedule.end());
-	
-	int time = schedule[0].first;
-	int count = 1;
-	for (int i = 1 ;i < N; i++) 
-	{
-		if (time <= schedule[i].second )
-		{
-			count++;
-			time = schedule[i].first;
-		}
-	}
-
-	cout << count;
+    std::ios::sync_with_stdio(false);  // 입출력 속도 최적화
+    std::cin.tie(NULL);                // 입출력 묶음 해제
+    std::cout.tie(NULL);                // 입출력 묶음 해제
+    
+    int N;
+    std::cin>>N;
+    
+    if (N == 0)
+    {
+        std::cout<<0;
+        return 0;
+    }
+    
+    std::vector<std::pair<int/*start*/, int/*end*/>> sortedList;
+    for(int i = 0; i < N; ++i)
+    {
+        int start, end;
+        std::cin>>start>>end;
+        sortedList.push_back({start, end});
+    }
+    
+    std::sort(sortedList.begin(), sortedList.end(), [](auto& lhs, auto& rhs){
+        if (lhs.second == rhs.second)
+        {
+            return lhs.first < rhs.first;
+        }
+        return lhs.second < rhs.second;
+    });
+    
+    int endTime = sortedList[0].second;
+    int cnt = 1;
+    for (int i = 1; i < N; ++i)
+    {
+        if (endTime <= sortedList[i].first)
+        {
+            cnt++;
+            endTime = sortedList[i].second;
+        }
+    }
+    std::cout<<cnt;
+    return 0;
 }
