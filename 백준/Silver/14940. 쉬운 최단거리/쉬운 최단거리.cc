d5f459f72918abe7fba312f9da5af855ab44d0c7
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-07 05:18:21 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 36 ms, Memory: 4140 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -1,56 +1,70 @@
-#include <iostream>
-#include <algorithm>
-#include <utility>
-#include <queue>
-using namespace std;
+#include<iostream>
+#include<vector>
+#include<queue>
 
+std::vector<std::pair<int, int>> dir = {{-1, 0},{0, 1},{1, 0},{0, -1}};
 
-int board[1002][1002];
-int dist[1002][1002];
-int dx[4] = { 0,0,1,-1 };
-int dy[4] = { 1,-1,0,0 };
-queue<pair<int, int>> q;
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			dist[i][j] = -1;
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 2) {
-				//cout << i << " , " << j << "\n";
-				q.push({ i,j });
-				dist[i][j] = 0;
-			}
-		}
-	}
-	
-	while (!q.empty()) {
-		auto cur = q.front();
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cur.first + dx[i];
-			int ny = cur.second + dy[i];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (dist[nx][ny] != -1) continue;
-			if (board[nx][ny] == 0) continue;
-			dist[nx][ny] = dist[cur.first][cur.second] + 1;
-			q.push({ nx,ny });
-		}
-	}
+int main()
+{
+    std::ios::sync_with_stdio(false);  // 입출력 속도 최적화
+    std::cin.tie(NULL);                // 입출력 묶음 해제
+    std::cout.tie(NULL);                // 입출력 묶음 해제
 
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (board[i][j] == 0) cout << 0 << " ";
-			else cout << dist[i][j] << " ";
-		}
-		cout << "\n";
-	}
+    int n, m;
+    std::cin>>n>>m;
+    
+    std::vector<std::vector<int>> map(n, std::vector<int>(m, 0));
+    std::vector<std::vector<int>> dist(n, std::vector<int>(m, -1));
+    int startY = -1;
+    int startX = -1;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin>>map[i][j];
+            if (map[i][j] == 0)
+            {
+                dist[i][j] = 0;
+            }
+            else if (map[i][j] == 2)
+            {
+                startY = i;
+                startX = j;
+            }
+        }
+    }
+    
+    std::queue<std::pair<int, int>> qu;
+    qu.push({startY, startX});
+    dist[startY][startX] = 0;
+    
+    while(!qu.empty())
+    {
+        auto curPos = qu.front();
+        qu.pop();
+        
+        for (auto& pos : dir)
+        {
+            int nextY = pos.first + curPos.first;
+            int nextX = pos.second + curPos.second;
+            
+            if (nextY < 0 || nextY >= n || nextX < 0 || nextX >= m || map[nextY][nextX] != 1)
+            {
+                continue;        
+            }
+            
+            qu.push({nextY, nextX});
+            map[nextY][nextX] = 2;
+            dist[nextY][nextX] = dist[curPos.first][curPos.second] + 1;
+        }
+    }
+    
+    for (int i = 0; i < n ; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cout<<dist[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
 }
