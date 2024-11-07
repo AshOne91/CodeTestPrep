814a09d1ee07d3d5079421200c0d1e6afafaa6e9
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-08 04:13:08 +0900

    [Gold V] Title: AC, Time: 44 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,73 +1,85 @@
+#include <iostream>
+#include <string>
+#include <deque>
+#include <algorithm>
 
-#include<iostream>
-#include<deque>
-#include<algorithm>
-#include<string>
-using namespace std;
-int t, n;
-string strp, strn;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> t;
-	while (t--)
-	{
-		cin >> strp;
-		cin >> n;
-		cin >> strn;
-		deque<int> dq;
-		bool flag = true; //error
-		bool dir = true; //정방향 역방향
-		string temp = "";
-		for (int i = 1; i < strn.size(); i++) //string to deque
-		{
-			if (strn[i] == ',' || strn[i] == ']')
-			{
-				if (temp != "")
-				{
-					dq.push_back(stoi(temp));
-					temp = "";
-				}
-			}
-			else
-			{
-				temp += strn[i];
-			}
-		}
-		for (int i = 0; i < strp.size(); i++)
-		{
-			if (strp[i] == 'R')
-			{
-				dir = !dir;
-			}
-			else if (strp[i] == 'D')
-			{
-				if (dq.empty())
-				{
-					flag = false;
-					break;
-				}
-				else//출력
-				{
-					if (dir) dq.pop_front();
-					else dq.pop_back();
-				}
-			}
-		}
-		if (!flag) cout << "error\n";
-		else
-		{
-			if (!dir) reverse(dq.begin(), dq.end());
-			cout << "[";
-			for (int i = 0; i < dq.size(); i++)
-			{
-				if (i == dq.size() - 1) 
-					cout << dq[i];
-				else
-					cout << dq[i] << ",";
-			}
-			cout << "]\n";
-		}
-	}
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    
+    int T;
+    std::cin >> T;
+    
+    for (int i = 0; i < T; ++i)
+    {
+        std::string cmd;
+        int N;
+        std::string str;
+        std::cin >> cmd;
+        std::cin >> N;
+        std::cin >> str;
+        
+        std::deque<int> dq;
+        bool dir = true;
+        bool flag = true;
+        
+        std::string temp;
+        for (int j = 1; j < (int)str.size(); ++j)
+        {
+            if (str[j] == ',' || str[j] == ']')
+            {
+                if (!temp.empty())
+                {
+                    dq.push_back(std::stoi(temp));
+                    temp.clear();
+                }
+            }
+            else
+            {
+                temp += str[j];
+            }
+        }
+        
+        for (int j = 0; j < (int)cmd.size(); ++j)
+        {
+            if (cmd[j] == 'R')
+            {
+                dir = !dir;
+            }
+            else if (cmd[j] == 'D')
+            {
+                if (dq.empty())
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                {
+                    if (dir)
+                        dq.pop_front();
+                    else
+                        dq.pop_back();
+                }
+            }
+        }
+        
+        if (!flag)
+            std::cout << "error\n";
+        else
+        {
+            if (!dir)
+                std::reverse(dq.begin(), dq.end());
+            
+            std::cout << "[";
+            for (int j = 0; j < (int)dq.size(); ++j)
+            {
+                std::cout << dq[j];
+                if (j < (int)dq.size() - 1)
+                    std::cout << ",";
+            }
+            std::cout << "]\n";
+        }
+    }
 }
