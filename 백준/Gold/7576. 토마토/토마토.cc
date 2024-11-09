3775f99f7fb9854d81c9cba55e2926a55aa8aebb
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-09 19:14:15 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 10216 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,81 +1,74 @@
-#include <iostream>
-#include <queue> // use bfs algorithm
-#include <stdio.h> // use printf, scanf
-#include <cstring> // use memset
- 
-#define MAX_SIZE 1000 + 1
- 
-using namespace std;
- 
-struct tomato {
-    int y, x;
-};
- 
-queue<tomato> q;
- 
-// 우,하,좌,상
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0 , -1 };
- 
-int n, m, result = 0;
-int graph[MAX_SIZE][MAX_SIZE];
- 
-bool IsInside(int ny, int nx) {
-    return (0 <= nx && 0 <= ny && nx < m && ny < n);
-}
- 
-void bfs(void) {
-    while (!q.empty()) {
-        // 큐의 현재 원소(익은 토마토(1))를 꺼내기
-        int y = q.front().y;
-        int x = q.front().x;
-        q.pop();
- 
-        // 해당 위치의 주변을 확인
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
- 
-            // 지도를 벗어나지 않고, 익지않은 토마토(0)라면
-            if (IsInside(ny,nx) == 1 && graph[ny][nx] == 0) {
-                graph[ny][nx] = graph[y][x] + 1;
-                q.push({ ny, nx });
+#include<iostream>
+#include<queue>
+int map[1001][1001];
+int dy[4] = { -1, 1, 0, 0};
+int dx[4] = { 0, 0, 1, -1};
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    
+    int M, N;
+    std::cin >> M >> N;
+    std::queue<std::pair<int, int>> qu;
+    bool allRipe = true;
+    
+    for (int y = 0; y < N; ++y)
+    {
+        for (int x = 0; x < M; ++x)
+        {
+            std::cin >> map[y][x];
+            if (map[y][x] == 1)
+            {
+                qu.push({y, x});
+            }
+            if (map[y][x] == 0)
+            {
+                allRipe = false;
             }
         }
     }
-}
- 
-int main() {
-    /* 토마토 농장(그래프)의 크기 입력 (가로/세로)*/
-    scanf("%d %d", &m, &n);
- 
-    /* 그래프 정보 입력*/
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &graph[i][j]);
-            if (graph[i][j] == 1) { // 익은토마토(1) -> 큐
-                q.push({ i, j });
+    
+    if (allRipe)
+    {
+        std::cout << 0 << "\n";
+        return 0;
+    }
+    
+    int maxDis = 0;
+    while(!qu.empty())
+    {
+        auto pos = qu.front();
+        qu.pop();
+        
+        for (int i = 0; i < 4; ++i)
+        {
+            int nextY = dy[i] + pos.first;
+            int nextX = dx[i] + pos.second;
+            if (nextY < 0 || nextX < 0 || nextY >= N || nextX >= M || map[nextY][nextX] != 0)
+            {
+                continue;
             }
+            
+            map[nextY][nextX] = map[pos.first][pos.second] + 1;
+            maxDis = std::max(maxDis, map[nextY][nextX]);
+            qu.push({nextY, nextX});
         }
     }
- 
-    /* 그래프를 BFS를 통해 탐색 */
-    bfs();
- 
-    /* 날짜 계산 및 출력  */
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            // 익지않은 토마토(0)가 존재할 경우
-            if (graph[i][j] == 0) { 
-                printf("-1\n");
+    
+    for (int y = 0; y < N; ++y)
+    {
+        for (int x = 0; x < M; ++x)
+        {
+            if (map[y][x] == 0)
+            {
+                std::cout << -1 << "\n";
                 return 0;
             }
-            // 토마토는 다 익었는데, 얼마만에 익었는지?
-            if (result < graph[i][j]) {
-                result = graph[i][j];
-            }
         }
     }
-    printf("%d\n", result-1);
+    
+    std::cout << (maxDis - 1) << "\n";
     return 0;
 }
