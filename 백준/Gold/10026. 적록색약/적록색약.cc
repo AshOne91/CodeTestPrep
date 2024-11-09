e5e6430a6ca835091847925f160e9e78b8edfb85
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-10 07:28:06 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,57 +1,69 @@
 #include <iostream>
-#include <queue>
-using namespace std;
+#include <stack>
+#include <string>
+#include <vector>
 
-struct xy {int x, y;};
-int n;
-int dx[] = {1, 0, -1, 0};
-int dy[] = {0, 1, 0, -1};
-char board[100][101];
-bool vis[100][100];
-queue<xy> Q;
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
 
-void bfs(int x, int y) {
-    char color = board[x][y];
-    vis[x][y] = true;
-    Q.push({x, y});
-    while(!Q.empty()) {
-        xy cur = Q.front(); Q.pop();
-        for(int d=4; d--;) {
-            int nx = cur.x + dx[d], ny = cur.y + dy[d];
-            if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
-            if(vis[nx][ny] || board[nx][ny] != color) continue;
-            vis[nx][ny] = true;
-            Q.push({nx, ny});
+inline bool isInBounds(int x, int y, int N) {
+    return x >= 0 && x < N && y >= 0 && y < N;
+}
+
+void dfs(int start, const char* map, char* visited, int N, char color) {
+    std::stack<int> stack;
+    stack.push(start);
+    visited[start] = 1;
+
+    while (!stack.empty()) {
+        int pos = stack.top();
+        stack.pop();
+
+        int x = pos / N, y = pos % N;
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int npos = nx * N + ny;
+            if (isInBounds(nx, ny, N) && !visited[npos] && map[npos] == color) {
+                visited[npos] = 1;
+                stack.push(npos);
+            }
         }
     }
 }
 
-int area() {
-    int cnt = 0;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(vis[i][j]) continue;
-            cnt++;
-            bfs(i, j);
+int countRegions(const char* map, int N) {
+    std::vector<char> visited(N * N, 0);
+    int count = 0;
+
+    for (int i = 0; i < N * N; ++i) {
+        if (!visited[i]) {
+            dfs(i, map, visited.data(), N, map[i]);
+            count++;
         }
     }
-    return cnt;
+    return count;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    int N;
+    std::cin >> N;
+    std::vector<char> normalMap(N * N);
+    std::vector<char> rgMap(N * N);
 
-    cin >> n;
-    for(int i=0; i<n; i++)
-        cin >> board[i];
-
-    cout << area() << ' ';
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(board[i][j] == 'G') board[i][j] = 'R';
-            if(vis[i][j]) vis[i][j] = false;
+    for (int i = 0; i < N; ++i) {
+        std::string str;
+        std::cin >> str;
+        for (int j = 0; j < N; ++j) {
+            char color = str[j];
+            normalMap[i * N + j] = color;
+            rgMap[i * N + j] = (color == 'R' || color == 'G') ? 'C' : color;
         }
     }
-    cout << area();
+
+    int normalCount = countRegions(normalMap.data(), N);
+    int rgCount = countRegions(rgMap.data(), N);
+
+    std::cout << normalCount << " " << rgCount << std::endl;
+    return 0;
 }
