d6bd92b6251ead98a268c1a904ef1adb7dc53361
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-13 07:28:43 +0900

    [Gold IV] Title: DSLR, Time: 3400 ms, Memory: 2316 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,59 +1,66 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <queue>
+
 int visited[10000];
-int A, B;
-void BFS();
- 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int T;
-	cin >> T;
-	while (T--) {
-		cin >> A >> B;
-		BFS();
-		memset(visited, 0, sizeof(visited));
-	}
-}
- 
-void BFS() {
-	queue<pair<int, string>> q;
-	q.push(make_pair(A, ""));
-	visited[A] = 1;
-	while (!q.empty()) {
-		int num = q.front().first;
-		string s = q.front().second;
-		if (num == B) {
-			cout << s << "\n";
-			return;
-		}
-		q.pop();
-		int D, S, L, R;
-		// D 연산
-		D = (num * 2) % 10000;
-		if (!visited[D]) {
-			visited[D] = 1;
-			q.push(make_pair(D, s + 'D'));
-		}
-		// S 연산
-		S = num - 1;
-		if (S == -1) S = 9999;
-		if (!visited[S]) {
-			visited[S] = 1;
-			q.push(make_pair(S, s + 'S'));
-		}
-		// L 연산
-		L = (num * 10) % 10000 + num / 1000;
-		if (!visited[L]) {
-			visited[L] = 1;
-			q.push(make_pair(L, s + 'L'));
-		}
-		// R 연산
-		R = num / 10 + (num % 10) * 1000;
-		if (!visited[R]) {
-			visited[R] = 1;
-			q.push(make_pair(R, s + 'R'));
-		}
-	}
+int visit_count = 1;
+
+int main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    
+    int T;
+    std::cin >> T;
+    while (T--)
+    {
+        std::queue<std::pair<int, std::string>> qu;
+        int A, B;
+        std::cin >> A >> B;
+        
+        qu.push({A, ""});
+        visited[A] = visit_count;
+
+        while (!qu.empty())
+        {
+            auto [num, cmd] = qu.front();
+            qu.pop();
+            
+            if (num == B)
+            {
+                std::cout << cmd << "\n";
+                break;
+            }
+            
+            int D = (num * 2) % 10000;
+            if (visited[D] != visit_count)
+            {
+                visited[D] = visit_count;
+                qu.push({D, cmd + 'D'});
+            }
+
+            int S = num == 0 ? 9999 : num - 1;
+            if (visited[S] != visit_count)
+            {
+                visited[S] = visit_count;
+                qu.push({S, cmd + 'S'});
+            }
+
+            int L = (num % 1000) * 10 + (num / 1000);
+            if (visited[L] != visit_count)
+            {
+                visited[L] = visit_count;
+                qu.push({L, cmd + 'L'});
+            }
+
+            int R = (num / 10) + (num % 10) * 1000;
+            if (visited[R] != visit_count)
+            {
+                visited[R] = visit_count;
+                qu.push({R, cmd + 'R'});
+            }
+        }
+        visit_count++;  // 다음 테스트 케이스에서 새 visit_count 사용
+    }
 }
