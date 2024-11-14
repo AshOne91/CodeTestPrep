0f2af8ec1b17b75c65f52e911fa6bcbee2861b96
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-15 04:38:26 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,30 +1,33 @@
 #include <iostream>
-#define MAX 9
+#include <vector>
 using namespace std;
 
-int n,m;
-int arr[MAX] = {0,};
-bool visited[MAX] = {0,};
+int N, M;
+vector<int> sequence;
 
-void dfs(int num, int cnt)
-{
-    if(cnt == m)
-    {
-        for(int i = 0; i < m; i++)
-            cout << arr[i] << ' ';
-        cout << '\n';
+void backtrack(int start) {
+    // 현재 길이가 M이라면 수열을 출력합니다.
+    if (sequence.size() == M) {
+        for (int num : sequence)
+            cout << num << " ";
+        cout << "\n";
         return;
     }
-    for(int i = num; i <= n; i++)
-    {
-            visited[i] = true;
-            arr[cnt] = i;
-            dfs(i,cnt+1);
-            visited[i] = false;
+    
+    // start부터 N까지의 숫자를 선택합니다.
+    for (int i = start; i <= N; ++i) {
+        sequence.push_back(i); // 숫자를 추가
+        backtrack(i);          // i부터 다시 시작하여 비내림차순 유지
+        sequence.pop_back();    // 백트래킹: 선택을 취소
     }
 }
 
 int main() {
-    cin >> n >> m;
-    dfs(1,0);
+    std::ios::sync_with_stdio(false);  // 입출력 속도 최적화
+    std::cin.tie(NULL);                // 입출력 묶음 해제
+    std::cout.tie(NULL);        
+    
+    cin >> N >> M;
+    backtrack(1); // 1부터 시작
+    return 0;
 }
