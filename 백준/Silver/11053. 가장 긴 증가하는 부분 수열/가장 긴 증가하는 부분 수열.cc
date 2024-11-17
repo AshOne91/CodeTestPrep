8eada96bfb3c4ede5f22a26f4ee2edc6cf61683d
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-17 14:31:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,30 +1,23 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-using namespace std;
 
 int main()
 {
     int N;
-    scanf("%d", &N);
+    std::cin >> N;
 
-    int arr[1001];
-    for (int i = 1; i <= N; i++)
-        scanf("%d", &arr[i]);
-
-    int dp[1001];
-    for (int i = 1; i <= N; i++)
-        dp[i] = 1;
-
-    for (int i = 1; i <= N; i++)
+    std::vector<int> lis; // LIS를 저장하는 배열
+    for (int i = 0; i < N; ++i)
     {
-        for (int j = 1; j < i; j++)
-        {
-            if(arr[i] > arr[j])
-            {
-                dp[i] = max(dp[i], dp[j]+1);
-            }
-        }
+        int x;
+        std::cin >> x;
+        auto it = std::lower_bound(lis.begin(), lis.end(), x);
+        if (it == lis.end())
+            lis.push_back(x);
+        else
+            *it = x;
     }
-    sort(dp, dp+N+1);
-    printf("%d",dp[N]);
+
+    std::cout << lis.size();
 }
