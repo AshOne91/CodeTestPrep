143dbb3bc557ff6ebbda70805323e9b7ea13c9ab
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-18 12:28:31 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 4900 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,36 +1,52 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
-
+#include <cstring>
 using namespace std;
-#define MAX 100001
 
-int N;
-int arr[MAX];
-bool visited[MAX];
-vector<int> v[MAX];
+const int MAX = 100000;
+int head[MAX + 1], to[MAX * 2], nextEdge[MAX * 2], parents[MAX + 1];
+int edgeCount = 0;
 
-void dfs(int k) {
-    visited[k]=true;
-    for(int i=0;i<v[k].size();i++) {
-        int next = v[k][i];
-        if(!visited[next]) {
-            arr[next]=k;
-            dfs(next);
+// 메모리를 덜 쓰는 그래프 연결 방식 (Adjacency List 직접 구현)
+void addEdge(int u, int v) {
+    to[edgeCount] = v;
+    nextEdge[edgeCount] = head[u];
+    head[u] = edgeCount++;
+}
+
+void dfs(int node) {
+    for (int edge = head[node]; edge != -1; edge = nextEdge[edge]) {
+        int neighbor = to[edge];
+        if (parents[neighbor] == 0) { // 아직 방문하지 않음
+            parents[neighbor] = node;
+            dfs(neighbor);
         }
     }
 }
 
 int main() {
-    cin >> N;
+    int n;
+    scanf("%d", &n);
 
-    for(int i=0;i<N;i++) {
-        int x,y;
-        cin >> x >> y;
-        v[x].push_back(y);
-        v[y].push_back(x);
+    // 초기화
+    memset(head, -1, sizeof(head));
+
+    // 입력 처리 및 간선 추가
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        addEdge(a, b);
+        addEdge(b, a);
     }
 
+    // DFS로 부모 찾기
+    parents[1] = 1; // 루트 노드 설정
     dfs(1);
 
-    for(int i=2;i<=N;i++) cout << arr[i] << "\n";
+    // 결과 출력
+    for (int i = 2; i <= n; ++i) {
+        printf("%d\n", parents[i]);
+    }
+
+    return 0;
 }
