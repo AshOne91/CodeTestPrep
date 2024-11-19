7f3eeb8b3b6eb7d28b55c4f869586b0d95dfbdbe
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-20 05:56:12 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -1,42 +1,22 @@
 #include <stdio.h>
 #include <algorithm>
-using namespace std;
-
-int n,m;
-
-int arr[10];
-int num[10];
-
-void seq(int x, int len){
-    if(len==m){
-        for(int i=0;i<m;i++)
-            printf("%d ",arr[i]);
+int n, m, arr[8], num[8];
+void seq(int x, int len) {
+    if (len == m) {
+        for (int i = 0; i < m; i++) printf("%d ", arr[i]);
         printf("\n");
         return;
     }
-
-    int last = 0;
-
-    for(int i=x;i<n;i++){
-        if(num[i]!=last){
-            arr[len] = num[i];
-            last = arr[len];
-            seq(i,len+1);
+    for (int i = x, last = 0; i < n; i++) {
+        if (num[i] != last) {
+            arr[len] = last = num[i];
+            seq(i, len + 1);
         }
     }
-
-    return;
 }
-
-int main(){
-    scanf("%d %d",&n,&m);
-
-    for(int i=0;i<n;i++)
-        scanf("%d",&num[i]);
-
-    sort(num,num+n);
-
-    seq(0,0);
-
-    return 0;
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) scanf("%d", &num[i]);
+    std::sort(num, num + n);
+    seq(0, 0);
 }
