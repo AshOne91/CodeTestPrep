9166296a963ee1146dfe5516772dbea2ecb6fc99
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2024-11-22 15:27:57 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 68 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,33 +1,55 @@
-#include <iostream>
+#include <cstdio>
 
-using namespace std;
+const int MAX_N = 1024;
+int sum[MAX_N + 1][MAX_N + 1];
 
-int arr[1025][1025],dp[1025][1025];
+// 빠른 입력 함수
+inline int fastInput() {
+    int x = 0, c;
+    while ((c = getchar_unlocked()) < '0');
+    do { x = x * 10 + (c - '0'); } while ((c = getchar_unlocked()) >= '0');
+    return x;
+}
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+// 빠른 출력 함수
+inline void fastOutput(int x) {
+    char buf[12];
+    int idx = 0;
+    if (x == 0) buf[idx++] = '0';
+    while (x > 0) {
+        buf[idx++] = (x % 10) + '0';
+        x /= 10;
+    }
+    while (idx--) putchar_unlocked(buf[idx]);
+    putchar_unlocked('\n');
+}
 
-    int n,m;
-    cin>>n>>m;
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            cin>>arr[i][j];
-            dp[i][j] = dp[i-1][j]+dp[i][j-1] - dp[i-1][j-1]+arr[i][j];
+int main() {
+    int N = fastInput();
+    int M = fastInput();
+
+    // 누적 합 배열 생성
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
+            int value = fastInput();
+            sum[i][j] = value + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
         }
     }
-    int x1,y1,x2,y2;
-    int ans;
-    for(int i=0;i<m;i++)
-    {
-        cin>>x1>>y1;
-        cin>>x2>>y2;
-        ans = dp[x2][y2] - dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1];
-        cout<<ans<<'\n';
+
+    // 쿼리 처리
+    while (M--) {
+        int x1 = fastInput();
+        int y1 = fastInput();
+        int x2 = fastInput();
+        int y2 = fastInput();
+
+        int result = sum[x2][y2]
+                     - (x1 > 1 ? sum[x1 - 1][y2] : 0)
+                     - (y1 > 1 ? sum[x2][y1 - 1] : 0)
+                     + (x1 > 1 && y1 > 1 ? sum[x1 - 1][y1 - 1] : 0);
+
+        fastOutput(result);
     }
+
     return 0;
 }
