f7f6682b0c4b7c167621c323f890fa4d3910b2fd
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-03-14 19:03:32 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 19740 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,36 +1,51 @@
-#include<iostream>
-#include<unordered_set>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
 
 int main()
 {
-    int N, M;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    int N; // 듣도 못한 사람의 수
+    int M; // 보도 못한 사람의 수
     std::cin>>N>>M;
+    std::unordered_map<std::string,int> mamber_map;
     
-    std::unordered_set<std::string> nList;
+    // 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름
     for (int i = 0; i < N; ++i)
     {
         std::string str;
         std::cin>>str;
-        nList.insert(str);
+        mamber_map[str]++;
     }
-
-    std::vector<std::string> result;
+    
     for (int i = 0; i < M; ++i)
     {
         std::string str;
         std::cin>>str;
-        if (nList.find(str) != nList.end())
+        mamber_map[str]++;
+    }
+    
+    std::vector<std::pair<std::string,int>> mamberList(mamber_map.begin(), mamber_map.end());
+    std::sort(mamberList.begin(), mamberList.end(), 
+              [](const std::pair<std::string, int>& left
+                 , const std::pair<std::string, int>& right){
+        return left.first < right.first;
+    });
+    
+    std::vector<std::string> results;
+    for(const auto& value : mamberList)
+    {
+        if (value.second == 2)
         {
-            result.push_back(str);
+            results.push_back({value.first});
         }
     }
-    std::sort(result.begin(), result.end());
-    
-    std::cout<<result.size()<<std::endl;
-    for(auto& value : result)
+    std::cout<<results.size()<<std::endl;
+    for(const auto& result : results)
     {
-        std::cout<<value<<std::endl;
+        std::cout<<result<<"\n";
     }
-    return 0;
 }
