afa9a5ce4e944ea4a166272e62a07cad89329fe8
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-03-16 10:58:18 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,30 +1,18 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 
-std::vector<std::pair<int, int>> dpMap(41);
+int dp[41][2] = {{1, 0}, {0, 1}}; 
 
-void countFibo(int n)
-{
-    for(int i = 2; i <= n; ++i)
-    {
-        dpMap[i] = std::make_pair((dpMap[i-1].first + dpMap[i-2].first),
-                                  (dpMap[i-1].second + dpMap[i-2].second));
+int main() {
+    std::ios::sync_with_stdio(0); 
+    std::cin.tie(0);
+
+    for (int i = 2; i <= 40; ++i) 
+        dp[i][0] = dp[i-1][0] + dp[i-2][0], dp[i][1] = dp[i-1][1] + dp[i-2][1];
+
+    int T, N;
+    std::cin >> T;
+    while (T--) {
+        std::cin >> N;
+        std::cout << dp[N][0] << ' ' << dp[N][1] << '\n';
     }
-}
-
-int main()
-{
-    int T;
-    std::cin>>T;
-    dpMap[0] = std::make_pair(1, 0);
-    dpMap[1] = std::make_pair(0, 1);
-    
-    for(int i = 0; i < T; ++i)
-    {
-        int N;
-        std::cin>>N;
-        countFibo(N);
-        std::cout<<dpMap[N].first <<' '<<dpMap[N].second<<"\n";
-    }
-    return 0;
 }
