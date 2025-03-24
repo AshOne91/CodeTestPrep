4f741bfbdbd03936a1f57fde7b33db47f34336e2
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-03-24 19:09:01 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 1176 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,35 +1,47 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int board[128][128];
-int ans[2];
+int ans[2];  // ans[0] - white, ans[1] - blue
 
-bool check(int n, int x, int y) {
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            if(board[x+i][y+j] != board[x][y])
-                return false;
-    return true;
+// 색이 모두 동일한지 확인
+int check(int n, int x, int y) {
+    int color = board[x][y];  // 첫 번째 색을 저장
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (board[x + i][y + j] != color) {
+                return 0;  // 색이 다르면 0 반환
+            }
+        }
+    }
+    return 1;  // 색이 모두 같으면 1 반환
 }
 
+// 분할 정복을 이용한 색 종이 카운트
 void solve(int n, int x, int y) {
-    if(check(n, x, y)) 
+    if (check(n, x, y)) {  // 동일한 색이면 바로 카운트
         ans[board[x][y]]++;
-    else {
-        n /= 2;
-        solve(n, x, y);
-        solve(n, x+n, y);
-        solve(n, x, y+n);
-        solve(n, x+n, y+n);
+    } else {
+        int half = n / 2;
+        solve(half, x, y);  // 좌상
+        solve(half, x + half, y);  // 우상
+        solve(half, x, y + half);  // 좌하
+        solve(half, x + half, y + half);  // 우하
     }
 }
 
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n; cin >> n;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            cin >> board[i][j];
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // 빠른 입력 처리
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            scanf("%d", &board[i][j]);
+        }
+    }
+
     solve(n, 0, 0);
-    for(int a:ans) cout << a << '\n';
+
+    printf("%d\n%d\n", ans[0], ans[1]);
+    return 0;
 }
