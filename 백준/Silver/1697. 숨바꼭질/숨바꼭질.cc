f9cbe1c84fc62d8af5acdc591a59bafeb1f526b2
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-03-29 18:37:52 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,51 +1,46 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-
-std::vector<int> checkResult(100001, 0);
+#include <iostream>
+#include <vector>
+#include <queue>
 
 int main()
 {
+    std::vector<bool> visited(100001, false);
     int N, K;
     std::cin>>N>>K;
     
-    std::queue<int> qu;
-    qu.push(N);
-    checkResult[N] = 0;
+    std::queue<std::pair<int, int>> qu;
+    qu.push({N, 0});
+    visited[N] = true;
     
-    while (!qu.empty())
+    while(!qu.empty())
     {
-        int curPos = qu.front();
+        auto pos = qu.front();
         qu.pop();
-        
-        if (curPos == K)
+        if (pos.first == K)
         {
-            break;
+            std::cout<<pos.second;
+            return 0;
         }
         
-        int minusPos = curPos - 1;
-        int plusPos = curPos + 1;
-        int multiPos = curPos * 2;
-        
-        if (minusPos >= 0 && checkResult[minusPos] == 0)
+        int nextPos = -1;
+        int nextDistance = pos.second + 1;
+        nextPos = pos.first - 1;
+        if (0 <= nextPos && nextPos <= 100000 && visited[nextPos] == false)
         {
-            qu.push(minusPos);
-            checkResult[minusPos] = checkResult[curPos] + 1;
+            visited[nextPos] = true;
+            qu.push({nextPos, nextDistance});
         }
-        
-        if (plusPos < 100001 && checkResult[plusPos] == 0)
+        nextPos = pos.first + 1;
+        if (0 <= nextPos && nextPos <= 100000 && visited[nextPos] == false)
         {
-            qu.push(plusPos);
-            checkResult[plusPos] = checkResult[curPos] + 1;
+            visited[nextPos] = true;
+            qu.push({nextPos, nextDistance});
         }
-        
-        if (multiPos < 100001 && checkResult[multiPos] == 0)
+        nextPos = pos.first * 2;
+        if (0 <= nextPos && nextPos <= 100000 && visited[nextPos] == false)
         {
-            qu.push(multiPos);
-            checkResult[multiPos] = checkResult[curPos] + 1;
+            visited[nextPos] = true;
+            qu.push({nextPos, nextDistance});
         }
     }
-    
-    std::cout<<checkResult[K];
 }
-
