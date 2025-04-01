dcc3c80adac125797e05db5c36e098ccbf223faa
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-04-01 14:31:43 +0900

    [Gold V] Title: 토마토, Time: 68 ms, Memory: 13764 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,74 +1,46 @@
-#include<iostream>
-#include<queue>
-int map[1001][1001];
-int dy[4] = { -1, 1, 0, 0};
-int dx[4] = { 0, 0, 1, -1};
-int main()
-{
+#include <iostream>
+
+#define MAX 1001
+int map[MAX][MAX], queue[MAX * MAX][2], front = 0, rear = 0;
+int dy[4] = {-1, 1, 0, 0}, dx[4] = {0, 0, 1, -1};
+
+int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
-    std::cout.tie(NULL);
     
     int M, N;
     std::cin >> M >> N;
-    std::queue<std::pair<int, int>> qu;
+    
     bool allRipe = true;
-    
-    for (int y = 0; y < N; ++y)
-    {
-        for (int x = 0; x < M; ++x)
-        {
+    for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < M; ++x) {
             std::cin >> map[y][x];
-            if (map[y][x] == 1)
-            {
-                qu.push({y, x});
-            }
-            if (map[y][x] == 0)
-            {
-                allRipe = false;
-            }
+            if (map[y][x] == 1) queue[rear][0] = y, queue[rear++][1] = x;
+            if (map[y][x] == 0) allRipe = false;
         }
     }
     
-    if (allRipe)
-    {
-        std::cout << 0 << "\n";
-        return 0;
-    }
+    if (allRipe) return std::cout << "0\n", 0;
     
-    int maxDis = 0;
-    while(!qu.empty())
-    {
-        auto pos = qu.front();
-        qu.pop();
-        
-        for (int i = 0; i < 4; ++i)
-        {
-            int nextY = dy[i] + pos.first;
-            int nextX = dx[i] + pos.second;
-            if (nextY < 0 || nextX < 0 || nextY >= N || nextX >= M || map[nextY][nextX] != 0)
-            {
-                continue;
+    int days = -1;
+    while (front < rear) {
+        int size = rear - front;
+        while (size--) {
+            int y = queue[front][0], x = queue[front++][1];
+            for (int i = 0; i < 4; ++i) {
+                int ny = y + dy[i], nx = x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= N || nx >= M || map[ny][nx] != 0) continue;
+                map[ny][nx] = 1;
+                queue[rear][0] = ny, queue[rear++][1] = nx;
             }
-            
-            map[nextY][nextX] = map[pos.first][pos.second] + 1;
-            maxDis = std::max(maxDis, map[nextY][nextX]);
-            qu.push({nextY, nextX});
         }
+        ++days;
     }
     
     for (int y = 0; y < N; ++y)
-    {
         for (int x = 0; x < M; ++x)
-        {
-            if (map[y][x] == 0)
-            {
-                std::cout << -1 << "\n";
-                return 0;
-            }
-        }
-    }
+            if (map[y][x] == 0) return std::cout << "-1\n", 0;
     
-    std::cout << (maxDis - 1) << "\n";
+    std::cout << days << "\n";
     return 0;
 }
