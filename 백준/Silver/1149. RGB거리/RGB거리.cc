a4156f053b5ab490d1cfcfe47b008917dbd0231b
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-04-07 18:19:39 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -4,28 +4,26 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 
     int N;
     cin >> N;
 
-    int prevR = 0, prevG = 0, prevB = 0; // 이전 집의 최솟값 저장
-    for (int i = 0; i < N; ++i) {
+    int prevR = 0, prevG = 0, prevB = 0;
+
+    while (N--) {
         int R, G, B;
         cin >> R >> G >> B;
 
-        // 현재 비용 계산 (이전 값 기반으로 업데이트)
-        int currR = R + min(prevG, prevB);
-        int currG = G + min(prevR, prevB);
-        int currB = B + min(prevR, prevG);
+        int nextR = R + min(prevG, prevB);
+        int nextG = G + min(prevR, prevB);
+        int nextB = B + min(prevR, prevG);
 
-        // 이전 값을 현재 값으로 업데이트
-        prevR = currR;
-        prevG = currG;
-        prevB = currB;
+        prevR = nextR;
+        prevG = nextG;
+        prevB = nextB;
     }
 
-    // 결과 출력: 마지막 집의 최소 비용
     cout << min({prevR, prevG, prevB}) << '\n';
     return 0;
 }
