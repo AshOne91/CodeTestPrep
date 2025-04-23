2642fe23875c54ee5b7811212317da8c57900994
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-04-23 15:43:35 +0900

    [Gold V] Title: LCS, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,38 +1,36 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
 
-using namespace std;
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-int longest_common_subsequence(const string& str1, const string& str2) {
-    int n = str1.size();
-    int m = str2.size();
+    std::string str_1, str_2;
+    std::cin >> str_1 >> str_2;
 
-    // DP 테이블 초기화
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    // 항상 str_1이 더 짧게
+    if (str_1.size() > str_2.size()) std::swap(str_1, str_2);
 
-    // DP 채우기
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (str1[i - 1] == str2[j - 1]) { // 문자가 같은 경우
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else { // 문자가 다른 경우
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
+    int n = str_1.size();
+    int m = str_2.size();
+    
+    std::vector<int> prev(m + 1, 0), curr(m + 1, 0);
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= m; ++j)
+        {
+            if (str_1[i - 1] == str_2[j - 1])
+                curr[j] = prev[j - 1] + 1;
+            else
+                curr[j] = std::max(prev[j], curr[j - 1]);
         }
+        std::swap(prev, curr);
     }
 
-    // 최종 결과 반환
-    return dp[n][m];
-}
-
-int main() {
-    string str1, str2;
-    cin >> str1 >> str2;
-
-    // LCS 길이 출력
-    cout << longest_common_subsequence(str1, str2) << endl;
-
-    return 0;
+    std::cout << prev[m];
 }
