855915ae45f19efb8a07491582462a7cec010b35
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-10-19 19:35:14 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,29 +1,7 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<numeric>
-
-int main()
-{
-    int N;
-    std::cin>>N;
-    
-    std::vector<float> totalScore;
-    for(int i = 0; i < N; ++i)
-    {
-        float score = 0;
-        std::cin>>score;
-        totalScore.push_back(score);
-    }
-    
-    float M = *std::max_element(totalScore.begin(), totalScore.end());
-    for(int i = 0; i < totalScore.size(); ++i)
-    {
-        totalScore[i] = totalScore[i] / M * 100.0f;
-    }
-    
-    float sum = std::accumulate(totalScore.begin(), totalScore.end(), 0.0f);
-    float avg = sum / totalScore.size();
-    std::cout<<avg<<std::endl;
-    return 0;
+#include <iostream>
+using namespace std;
+int main(){
+int n;double m=0,s=0,x;cin>>n;
+for(int i=0;i<n;i++){cin>>x;if(x>m)m=x;s+=x;}
+cout<<s/m*100/n;
 }
