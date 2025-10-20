eb68cab6b03a3a3e4261a5a5c8cdbde230452381
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-10-20 16:45:10 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,40 +1,43 @@
-#include<iostream>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <queue>
+using namespace std;
 
-int main()
-{
+int main() {
+    /*
+    Fast I/O
+
+    N이 최대 5,000으로 작지만
+    습관적으로 사용
+    */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int N, K;
-    std::cin>>N>>K;
-    std::queue<int> qu;
-    for(int i = 0; i < N; ++i)
-    {
-        qu.push(i + 1);
+    cin >> N >> K;
+
+    queue<int> q;
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
     }
-    
-    std::vector<int> resultList;
-    while(!qu.empty())
-    {
-        for(int i = 0; i < K-1; ++i)
-        {
-            int num = qu.front();
-            qu.pop();
-            qu.push(num);
+
+    cout << "<";
+    bool first = true;  // 첫 번째 원소 체크
+
+    while (!q.empty()) {
+        for (int i = 0; i < K - 1; i++) {
+            q.push(q.front());  // 맨 앞을 맨 뒤로
+            q.pop();            // 맨 앞 제거
         }
-        
-        resultList.push_back(qu.front());
-        qu.pop();
+
+        if (!first) {
+            cout << ", ";
+        }
+        cout << q.front();  // K번째 사람 출력
+        q.pop();            // 제거
+
+        first = false;
     }
-    
-    std::cout<<"<";
-    for(int i = 0; i < resultList.size() - 1; ++i)
-    {
-        std::cout<<resultList[i]<<", ";
-    }
-    if (resultList.size() > 0)
-    {
-        std::cout<<resultList[resultList.size()-1];
-    }
-    std::cout<<">";
+
+    cout << ">";
     return 0;
 }
