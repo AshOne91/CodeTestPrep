518e2e73b0c977f217675176034a1fbcf4967c26
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-10-20 12:59:46 +0900

    [Silver IV] Title: 수 찾기, Time: 60 ms, Memory: 6820 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,35 +1,27 @@
-#include<iostream>
-#include<unordered_set>
+#include <iostream>
+#include <unordered_set>
+
 int main()
 {
-    std::ios::sync_with_stdio(false);  // 입출력 속도 최적화
-    std::cin.tie(NULL);                // 입출력 묶음 해제
-    std::cout.tie(NULL);                // 입출력 묶음 해제
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
     int N;
     std::cin>>N;
-    std::unordered_set<int> numList;
-    for(int i = 0; i < N; ++i)
+    std::unordered_set<int> numbers;
+    for (int i = 0; i < N; ++i)
     {
         int num;
         std::cin>>num;
-        numList.insert(num);
+        numbers.insert(num);
     }
-    
     int M;
     std::cin>>M;
-    for(int i = 0; i < M; ++i)
+    for (int i = 0; i < M; ++i)
     {
         int num;
         std::cin>>num;
-        if (numList.find(num) == numList.end())
-        {
-            std::cout<<0<<"\n";
-        }
-        else
-        {
-            std::cout<<1<<"\n";
-        }
+        std::cout<<(numbers.find(num) != numbers.end() ? 1 : 0)<<'\n';
     }
-    return 0;
 }
