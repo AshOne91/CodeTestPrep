a388e3e9fee60f01860cd4b7573ca42c7f2488ce
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-10-21 17:03:25 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,54 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+using namespace std;
 
-const int INF = std::numeric_limits<int>::max();
+const int INF = numeric_limits<int>::max();
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-    
-    int n;
-    std::cin>>n;
-    std::vector<std::vector<int>> dp(n+1, std::vector<int>(n+1, INF));
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n;
+
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, INF));
     for (int i = 1; i <= n; ++i)
-    {
         dp[i][i] = 0;
-    }
-    
-    int m;
-    std::cin>>m;
+
+    cin >> m;
     for (int i = 0; i < m; ++i)
     {
         int a, b, c;
-        std::cin >> a >> b >> c;
-        dp[a][b] = std::min(dp[a][b], c);
+        cin >> a >> b >> c;
+        dp[a][b] = min(dp[a][b], c);
     }
 
     for (int k = 1; k <= n; ++k)
-    {
         for (int i = 1; i <= n; ++i)
-        {
             for (int j = 1; j <= n; ++j)
-            {
                 if (dp[i][k] != INF && dp[k][j] != INF)
-                    dp[i][j] = std::min(dp[i][j], dp[i][k] + dp[k][j]);
-            }
-        }
-    }
-    
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+
     for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= n; ++j)
-        {
-            if (dp[i][j] == INF)
-            {
-               dp[i][j] = 0; 
-            }
-            std::cout<<dp[i][j]<<' ';
-        }
-        std::cout<<'\n';
+            cout << (dp[i][j] == INF ? 0 : dp[i][j]) << ' ';
+        cout << '\n';
     }
+
+    return 0;
 }
