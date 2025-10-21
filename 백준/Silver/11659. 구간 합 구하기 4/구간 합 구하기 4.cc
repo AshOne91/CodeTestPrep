8ce4c3abe6a4b2713883f315a969315ad686d09e
AshOne91 <103973741+AshOne91@users.noreply.github.com>
2025-10-21 13:05:14 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -3,28 +3,25 @@
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cout.tie(NULL);
-    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
     
     int N, M;
     std::cin>>N>>M;
-    
     std::vector<int> sum(N + 1, 0);
     for (int i = 1; i <= N; ++i)
     {
         std::cin>>sum[i];
+        sum[i] = sum[i] + sum[i - 1];
     }
-    
-    for (int i = 1; i <= N - 1; ++i)
-    {
-        sum[i + 1] = sum[i] + sum[i + 1];
-    }
-    
+
     for (int i = 0; i < M; ++i)
     {
         int start, end;
         std::cin>>start>>end;
         std::cout<<sum[end] - sum[start - 1]<<'\n';
     }
+    return 0;
 }
